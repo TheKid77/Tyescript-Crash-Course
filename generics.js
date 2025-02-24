d5f953b87d5c5a3702e382e8e36db0ca92733d54
TheKid77 <andymcc77@blueyonder.co.uk>
2025-02-24 13:34:40 +0000

    Add Generics File

---
+++ generics.js
@@ -0,0 +1,12 @@
+const bookPageCounts = [256, 430, 480, 192, 400];
+const popularGenres = ["mystery", "romance", "science fiction", "fantasy"];
+const authors = [
+    { name: "Jane", booksPublished: 5 },
+    { name: "Mark", booksPublished: 12 },
+];
+function getLastItem(array) {
+    return array[array.length - 1];
+}
+console.log(getLastItem(bookPageCounts));
+console.log(getLastItem(popularGenres));
+console.log(getLastItem(authors));
